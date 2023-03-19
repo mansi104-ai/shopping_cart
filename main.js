dd2af68dfb004dd0e6d9ff9dcee283653b603edd
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-03-19 21:29:45 +0530

    error in price identifier

--- main.js
+++ main.js
@@ -11,19 +11,19 @@ let shopItemsData= [{
     id : "ansjsjxbj",
     name : "Office shirt"
     price: 110,
-    desc :"Lorem "
+    desc :"ooii"
     img: "img2.jpg"
 },{
     id : "zjjdekekd",
     name : "Pretty Frock"
     price: 90,
-    desc :"Lorem "
+    desc :"iiijij "
     img: "img3.jpg"
 },{
     id : "irirjfn",
     name : "Hat"
     price: 20,
-    desc :"Lorem "
+    desc :"mkmmk "
     img: "img4.jpg"
 }];
 
@@ -33,21 +33,22 @@ let generateShop=() =>{
     return (shop.innerHTML = shopItemsData.map((x)=>{
         let {id,name,price,desc} = x;
         return  `
-        <div class="item">
+    <div id = product-id-${id} class="item">
         <img width="200" src=${img} alt ="">
         <div class="details">
             <h3>${name}</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p>${desc} </p>
             <div class="price-quantity">
                 <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i> 
+                    <i  onclick ="decrement()" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">0</div>
+                    <i onclick ="incerement()" class="bi bi-plus-lg"></i> 
                 </div>
             </div>
         </div>
         </div>
+        ;
           `     
           
     
@@ -59,3 +60,11 @@ let generateShop=() =>{
 };
 
 generateShop();
+
+let increment = ()=>{
+    console.log("increment")
+};
+let decrement = ()=>{
+    console.log("decrement")
+};
+let update = () =>{};
