3886beb8c649273ff1c69cb09b2cc744dba95367
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-03-19 21:00:15 +0530

    basic images added

--- main.js
+++ main.js
@@ -1,91 +1,61 @@
-let shop = document.getElementById('shop')
+let shop = document.getElementById('shop');
 
-let shopItemsData= [
-     {
-
-    id: "jdkgfjnjfg",
-    name: "Casual Shirt",
-    price: "$45" ,
-    desc:"lorem ipsum ",
+let shopItemsData= [{
+    id : "gfjdfbdjb",
+    name : "Casual Shirt"
+    price: 45,
+    desc :"Lorem "
     img: "img1.jpg"
-
-    },
-    {
-    id: "fjdnvjjdj",
-    name: "Frock",
-    price:  "$80",
-    desc:"Dark Blue ",
+    
+},{
+    id : "ansjsjxbj",
+    name : "Office shirt"
+    price: 110,
+    desc :"Lorem "
     img: "img2.jpg"
-    },
-    {
-    id: "nnvnfvnfjv",
-    name: "Blazer",
-    price:  "$100",
-    desc:"Black",
+},{
+    id : "zjjdekekd",
+    name : "Pretty Frock"
+    price: 90,
+    desc :"Lorem "
     img: "img3.jpg"
-    },
-    {
-    id: "ieieie",
-    name: "Hat",
-    price:  "$20",
-    desc:"burnt sienna",
+},{
+    id : "irirjfn",
+    name : "Hat"
+    price: 20,
+    desc :"Lorem "
     img: "img4.jpg"
-    }
-];
-console.log {shop};
+}];
 
-let basket=[
-    
-    
-];
 
-let generateShop =() => {
-    return (shop.innerHTML= shopItemsData
-        .map((x)=>{
-        let{id, name , price, desc, img} = x;
-        return `
-    <div id= product-id-${id} class="item">
-        <img width="220" src=${img} alt="">
+
+let generateShop=() =>{
+    return (shop.innerHTML = shopItemsData.map((x)=>{
+        let {id,name,price,desc} = x;
+        return  `
+        <div class="item">
+        <img width="200" src=${img} alt ="">
         <div class="details">
             <h3>${name}</h3>
-            <p>${desc} </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
             <div class="price-quantity">
-                <h2>${price}</h2>
+                <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-                    <div id=${id} class="quantity">0</div>
-                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                    <i class="bi bi-dash-lg"></i>
+                    <div class="quantity">0</div>
+                    <i class="bi bi-plus-lg"></i> 
                 </div>
             </div>
         </div>
-    </div>
-`
-    }).join(" "))
+        </div>
+          `     
+          
+    
+              
+     
+        }).join(""));
     
     
 };
 
 generateShop();
-
-let increment = (id)=>{
-    let selectedItem = id;
-    let search = basket.find((x)=>x.id === selectedItem);
-    
-    if (search === undefined){
-        basket.push({
-            id: selectedItem.id,
-            item:1,
-
-    });
-}; 
-    else {
-    search.item +=1;
-};
-
-    console.log(basket);
-};
-let decrement = (id)=>{
-    let selectedItem = id;
-    console.log(selectedItem.id)
-};
-let update = () =>{};
