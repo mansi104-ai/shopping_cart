8ab538305b5a09eb7380b4f62e62f436e3c7e004
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-03-31 20:44:25 +0530

    generateShop added

--- main.js
+++ main.js
@@ -1,70 +1,25 @@
-let shop = document.getElementById('shop');
+let shop=document.getElementById('shop');
 
-let shopItemsData= [{
-    id : "gfjdfbdjb",
-    name : "Casual Shirt"
-    price: 45,
-    desc :"Lorem "
-    img: "img1.jpg"
-    
-},{
-    id : "ansjsjxbj",
-    name : "Office shirt"
-    price: 110,
-    desc :"ooii"
-    img: "img2.jpg"
-},{
-    id : "zjjdekekd",
-    name : "Pretty Frock"
-    price: 90,
-    desc :"iiijij "
-    img: "img3.jpg"
-},{
-    id : "irirjfn",
-    name : "Hat"
-    price: 20,
-    desc :"mkmmk "
-    img: "img4.jpg"
-}];
+let generateShop=()=>{
+    return shop.innerHTML= `
+    <div class="item">
+            <img width ="220"src="img1.jpg" alt="">
+            <div class="details">
+                <h3>Casual Shirt</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                <div class="price-quantity">
+                    <h2>$45</h2>
+                    <div class="buttons">
+                        <i class="bi bi-dash-circle-fill"></i>
+                        <div class="quantity">0</div>
+                        <i class="bi bi-plus-circle-fill"></i>
 
-
-
-let generateShop=() =>{
-    return (shop.innerHTML = shopItemsData.map((x)=>{
-        let {id,name,price,desc} = x;
-        return  `
-    <div id = product-id-${id} class="item">
-        <img width="200" src=${img} alt ="">
-        <div class="details">
-            <h3>${name}</h3>
-            <p>${desc} </p>
-            <div class="price-quantity">
-                <h2>$ ${price}</h2>
-                <div class="buttons">
-                    <i  onclick ="decrement()" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
-                    <i onclick ="incerement()" class="bi bi-plus-lg"></i> 
+                    </div>
                 </div>
+
             </div>
         </div>
-        </div>
-        ;
-          `     
-          
-    
-              
-     
-        }).join(""));
-    
-    
-};
+    `
+}
 
 generateShop();
-
-let increment = ()=>{
-    console.log("increment")
-};
-let decrement = ()=>{
-    console.log("decrement")
-};
-let update = () =>{};
