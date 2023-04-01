6502f46e938463a2bbb176d5018221d5374e819b
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-04-01 15:50:19 +0530

    checkout page and total bill added

--- main.js
+++ main.js
@@ -1,35 +1,6 @@
 let shop=document.getElementById('shop');
 
-let shopItemsData=[{
-    id:"ifvvvnofvn",
-    name:"Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    img: "img1.jpg"
-},
-{
-    id:"peoeoeoeo",
-    name:"Pretty Frock",
-    price: 100,
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    img: "img2.jpg"
 
-},
-{
-    id:"rurririuir",
-    name:"Blazer",
-    price: 200,
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    img: "img3.jpg"
-
-},
-{
-    id:"pproeir",
-    name:"Hat",
-    price: 20,
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    img: "img4.jpg"
-}];
 
 let basket= JSON.parse(localStorage.getItem("data"))|| []
 
